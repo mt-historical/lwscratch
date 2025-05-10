94ef91f4c75b5d8a4c8c2038f1ee96736193c97a
maplemedley <198984680+maplemedley@users.noreply.github.com>
2025-05-10 14:03:42 +0200

    Add a prefix for robot-sent messages
    This is both to make the chat more easily interpretable, and to prevent things like impersonation.
    
    The messages use `[]` instead of `<>` to make them quickly distinguishable from player messages.
    
    The idea is that if `<>` were used, every time a robot sent a message, it would seem like a player sent that message, and players would have to read the username to see that it was a robot. Using `[]` should make it so a quick glance is enough to make clear the message isn't from a player, instead of making users read the username for every new could-be-player message.
    
    In short it makes it clear and at-a-glance distinguishable which messages are human and which are from a robot, preventing possible frustrations and UX issues that would come from muddying up the chat with either the lack of a prefix, or the player-chat prefix, while still keeping it simple.

--- program.lua
+++ program.lua
@@ -1215,6 +1215,16 @@ local function run_chat (program, robot_pos)
 		message = tostring (program:get_value (item))
 	end
 
+	local meta = minetest.get_meta (robot_pos)
+	local robot_name = meta:get_string ("name")
+	if robot_name ~= "" and not string.match (robot_name, '^%..*$') then
+		message = "[robot " .. robot_name .. "] " .. message
+	else
+		-- Prepend minimally if name is empty or starts with "."
+		-- Note: Not allowing non-prefixed messages to prevent it being used for impersonation. 
+		message = "[robot] " .. message
+	end
+
 	utils.robot_chat (robot_pos, message)
 
 	return true
