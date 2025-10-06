a916b8b4869753bab159452a2857d899e74fb208
maplemedley <198984680+maplemedley@users.noreply.github.com>
2025-10-06 10:48:41 +0200

    Remove simplified recipe alternatives

--- crafting.lua
+++ crafting.lua
@@ -1,21 +1,5 @@
 local mats = xcompat.materials
 
--- Easier to remember recipes (ComputerCraft-inspired) 
-minetest.register_craft({
-	output = "lwscratch:cassette 1", 
-	recipe = { mats.book, mats.copper_ingot, mats.mese_crystal },
-	type   = "shapeless"
-})
-minetest.register_craft({
-	output = "lwscratch:robot 1", 
-	recipe = {
-		{ mats.copper_ingot, mats.copper_ingot, mats.copper_ingot },
-		{ mats.copper_ingot, mats.mese_crystal, mats.copper_ingot },
-		{ mats.copper_ingot, mats.chest,        mats.copper_ingot }
-	}
-})
-
--- Legacy recipes
 minetest.register_craft({
 	output = "lwscratch:cassette 1", 
 	recipe = { mats.clay_lump, mats.coal_lump, mats.stick, mats.book },
