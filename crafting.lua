bfeeb6cc962a09a4a6951018a7a9172e40f8a011
Medley <198984680+maplemedley@users.noreply.github.com>
2025-10-06 23:43:31 +0200

    Rework game-specific recipe support (#4)
    * Rework game-specific recipe support & add parity
    
    * Add more intuitive recipe alternatives
    
    These recipes are intended to be easier to remember and to require less
    inventory space occupied by crafting recipes to create.
    
    I found myself always forgetting the old recipes, and that I had to
    rummage through five different chests to get the necessary ingredients.
    These recipes keep it about as expensive as before, but with much more
    memorable and intuitive recipes. They're inspired by computercraft but
    uses copper instead, because of coppers connection with electronics and
    steampunk, and additionally because Minecraft is now associating copper
    with robots as well, another bonus for its intuitiveness.
    
    I think it's best to keep the old recipes alongside these new ones for
    old players who remember them. Both recipes are similarly cheap.
    
    * Fix missing items
    
    * Add fallback items for edge-cases
    
    * Use xcompat for recipe compatibility
    
    * Remove simplified recipe alternatives

--- crafting.lua
+++ crafting.lua
@@ -1,50 +1,23 @@
+local mats = xcompat.materials
 
-if minetest.global_exists ("default") then
-
-
-	minetest.register_craft({
-		output = "lwscratch:robot 1",
-		recipe = {
-			{ "default:stone", "default:tin_ingot", "default:glass" },
-			{ "default:steel_ingot", "default:coal_lump", "default:steel_ingot" },
-			{ "default:stick", "default:copper_ingot", "default:clay_lump" }
-		}
-	})
-
-
-	minetest.register_craft({
-		output = "lwscratch:cassette 1",
-		recipe = {
-			{ "default:clay_lump", "default:coal_lump" },
-			{ "default:stick", "default:book" }
-		}
-	})
-
-else
-
-	local game_id = Settings (minetest.get_worldpath ()..DIR_DELIM..'world.mt'):get ('gameid')
-
-	if game_id == "mineclone2" or game_id == "mineclonia" or game_id == "voxelibre"  then 
-		minetest.register_craft({
-			output = "lwscratch:robot 1",
-			recipe = {
-				{ "mcl_core:stone",      "mcl_core:iron_ingot", "mcl_core:glass" },
-				{ "mcl_core:iron_ingot", "mcl_core:coal_lump",  "mcl_core:iron_ingot" },
-				{ "mcl_core:stick",      "mcl_core:iron_ingot", "mcl_core:clay_lump" }
-			}
-		})
-		minetest.register_craft({
-			output = "lwscratch:cassette 1",
-			recipe = {
-				{ "mcl_core:clay_lump", "mcl_core:coal_lump" },
-				{ "mcl_core:stick",     "mcl_books:book" }
-			}
-		})
-	end
-
-
-end
-
-
-
---
+minetest.register_craft({
+	output = "lwscratch:cassette 1", 
+	recipe = { mats.clay_lump, mats.coal_lump, mats.stick, mats.book },
+	type   = "shapeless"
+})
+minetest.register_craft({
+	output = "lwscratch:robot 1", 
+	recipe = {
+		{ mats.stone,       mats.tin_ingot,    mats.glass },
+		{ mats.steel_ingot, mats.coal_lump,    mats.steel_ingot },
+		{ mats.stick,       mats.copper_ingot, mats.clay_lump }
+	}
+})
+minetest.register_craft({
+	output = "lwscratch:robot 1", 
+	recipe = {
+		{ mats.stone,       mats.steel_ingot, mats.glass },
+		{ mats.steel_ingot, mats.coal_lump,   mats.steel_ingot },
+		{ mats.stick,       mats.steel_ingot, mats.clay_lump }
+	}
+})
